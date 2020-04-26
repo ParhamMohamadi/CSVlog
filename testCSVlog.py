6980b18e06f664b7727eb29047208b7b8b2215e4
parham mohamadi <parham.mohamadi@gmail.com>
2020-04-27 02:22:34 +0430

    adding __del__ to test code

--- testCSVlog.py
+++ testCSVlog.py
@@ -2,4 +2,5 @@ import CSVlog
 import time
 
 MyTestData = CSVlog.DataCSVlogger('TestData')
-MyTestData.WriteData(time.strftime("%d/%m/%Y-%H:%M"), 1, 20)
+MyTestData.WriteData(time.strftime("%d/%m/%Y-%H:%M"), 1, 200)
+MyTestData.__del__()
